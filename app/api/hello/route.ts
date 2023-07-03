25c029de28456a0bab7237a22c67679b8eb5a2c5
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-07-03 22:31:23 +0530

    fix

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -2,25 +2,9 @@ import { MongoClient } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-	// Replace the uri string with your connection string.
-	const uri = "mongodb://my mongodb uri";
-
-	const client = new MongoClient(uri);
-
-	const database = client.db("test");
-	const values = database.collection("values");
-
-	// Query for a movie that has the title 'Back to the Future'
-	const doc = await values.findOne({ name: "counter" });
-	const value = doc!.value;
-	console.log(value);
-
-	// Ensures that the client will close when you finish/error
-	await client.close();
-
 	return NextResponse.json(
 		{
-			value,
+			message: "Hello World!",
 		},
 		{
 			status: 200,
