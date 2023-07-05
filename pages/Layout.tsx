e92bbf0f34407ef8e7984c2d6b284444c7ab3e7f
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-05 19:03:19 +0530

    the problem giver code is inserted

--- pages/Layout.tsx
+++ pages/Layout.tsx
@@ -6,23 +6,52 @@ import { MySession } from "../lib/types";
 import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-	const session = useSession();
+  const session = useSession();
 
-	const router = useRouter();
-	if (session.status === "unauthenticated") {
-		router.push("/");
-	}
-	return (
-		<>
-			<Head>
-				<title>Enchanted Oasis</title>
-				<meta name="description" content="Cool! " />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			Yo
-			<main>
-				<div>{children}</div>
-			</main>
-		</>
-	);
+  const router = useRouter();
+  if (session.status === "unauthenticated") {
+    router.push("/");
+  }
+  return (
+    <>
+      <Head>
+        <title>Enchanted Oasis</title>
+        <meta name="description" content="Cool! " />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Yo
+      <main>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" noWrap>
+              Material-UI
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+
+        <div>{children}</div>
+      </main>
+    </>
+  );
 }
