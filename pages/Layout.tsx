841ce655d2feb4284d3db89f0b8e8cd74ebb7c02
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-07-05 19:38:56 +0530

    removed mui

--- pages/Layout.tsx
+++ pages/Layout.tsx
@@ -2,8 +2,6 @@ import Head from "next/head";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { MySession } from "../lib/types";
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const session = useSession();
@@ -21,35 +19,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       </Head>
       Yo
       <main>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="open drawer"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography className={classes.title} variant="h6" noWrap>
-              Material-UI
-            </Typography>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                inputProps={{ "aria-label": "search" }}
-              />
-            </div>
-          </Toolbar>
-        </AppBar>
-
         <div>{children}</div>
       </main>
     </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,25 +2,16 @@ import { SessionProvider } from "next-auth/react";
 
 import type { AppProps } from "next/app";
 import type { Session } from "next-auth";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-const darkTheme = createTheme({
-	palette: {
-		mode: "dark",
-	},
-});
+
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({
-	Component,
-	pageProps: { session, ...pageProps },
+  Component,
+  pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
-	return (
-		<SessionProvider session={session}>
-			<ThemeProvider theme={darkTheme}>
-				<Component {...pageProps} />
-				<CssBaseline />
-			</ThemeProvider>
-		</SessionProvider>
-	);
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
