38fa4f363455a169caa87304bd8a33ccdff231fa
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-06 08:03:07 +0530

    navbar is decent for now

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -11,17 +11,42 @@ import {
   FormControl,
   FormLabel,
   Input,
+  InputGroup,
+  InputRightElement,
+  Avatar,
 } from "@chakra-ui/react";
 import React from "react";
 import styles from "./Nav.module.css";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, PhoneIcon } from "@chakra-ui/icons";
 
 const Nav = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <div>
-      
+    <div className={styles.navbar}>
+      {/* Hamburger Icon */}
+      <div className={styles.hamButton}>
+        <Button onClick={onOpen}>
+          <HamburgerIcon />
+        </Button>
+      </div>
+
+      {/* Search Bar */}
+      <div className={styles.searchBar}>
+        <FormControl id="search" >
+          <InputGroup>
+            <Input type="text" placeholder="Search" />
+            <InputRightElement pointerEvents="none">
+              <PhoneIcon color="gray.300" />
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+      </div>
+
+      {/* Avatar */}
+      <div>
+        <Avatar bg="red.500" />
+      </div>
     </div>
   );
 };
