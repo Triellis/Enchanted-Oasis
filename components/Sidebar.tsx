d5c4498f4e848006f9e26be1aca74463f5aea91e
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-06 22:11:49 +0530

    now have to make it responsive to each other

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,16 +4,14 @@ import styles from "./Sidebar.module.css";
 
 function Sidebar({ isOpen }: { isOpen: boolean }) {
   return (
-    <>
-      <aside className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
-        {/* Sidebar content goes here */}
-        <ul className={styles.menu}>
-          <li>Menu Item 1</li>
-          <li>Menu Item 2</li>
-          <li>Menu Item 3</li>
-        </ul>
-      </aside>
-    </>
+    <div className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
+      {/* Sidebar content goes here */}
+      <ul className={styles.menu}>
+        <li>Menu Item 1</li>
+        <li>Menu Item 2</li>
+        <li>Menu Item 3</li>
+      </ul>
+    </div>
   );
 }
 
