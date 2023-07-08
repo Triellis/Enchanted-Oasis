03ad1b36bd1038bac1da90c01989d78bdf9298e7
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-07-08 13:45:27 +0530

    slight changes

--- lib/theme.ts
+++ lib/theme.ts
@@ -2,6 +2,7 @@
 
 // 1. import `extendTheme` function
 import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import { defineStyleConfig } from "@chakra-ui/react";
 
 // 2. Add your color mode config
 const config: ThemeConfig = {
@@ -9,9 +10,20 @@ const config: ThemeConfig = {
   useSystemColorMode: false,
 };
 
+const Button = defineStyleConfig({
+  // The styles all button have in common
+  baseStyle: {
+    borderRadius: "var(--rounded-btn)", // <-- border radius is same for all variants and sizes
+  },
+  // Two sizes: sm and md
+});
+
 // 3. extend the theme
 const theme = extendTheme({
   config,
+  components: {
+    Button,
+  },
 });
 
 export { theme };
