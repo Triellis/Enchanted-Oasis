bad9ec83eee040cb2aac1e7c0a6147de59a37008
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-08 17:57:04 +0530

    sign in form redesigned!

--- pages/Auth/Signin.tsx
+++ pages/Auth/Signin.tsx
@@ -7,9 +7,16 @@ import { getCsrfToken } from "next-auth/react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { useToast } from "@chakra-ui/react";
+import {
+  Button,
+  Input,
+  InputGroup,
+  InputRightElement,
+  useToast,
+} from "@chakra-ui/react";
 
 import styles from "./Signin.module.css";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
 export default function SignIn({
   csrfToken,
@@ -19,52 +26,66 @@ export default function SignIn({
   const toast = useToast();
   const router = useRouter();
 
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
   return (
+    // main container of the form
     <div className={styles.container}>
       <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
 
-      <label>
-        Email
-        <input
-          name="Email"
-          type="text"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </label>
-      <label>
-        Password
-        <input
-          name="password"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </label>
-      <button
-        onClick={async () => {
-          const res = await signIn("credentials", {
-            email,
-            password,
-            redirect: false,
-          });
+      {/* text input part */}
+      <div className={styles.fillForm}>
+        <label>
+          <p>Email</p>
+          <InputGroup size="md">
+            <Input pr="4.5rem" placeholder="Enter email" />
+          </InputGroup>
+        </label>
+        <label>
+          <p>Password</p>
+          <InputGroup size="md">
+            <Input
+              pr="4.5rem"
+              type={show ? "text" : "password"}
+              placeholder="Enter password"
+            />
+            <InputRightElement width="3rem">
+              <Button size="sm" onClick={handleClick}>
+                {!show ? <ViewIcon /> : <ViewOffIcon />}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </label>
 
-          console.log(res);
+        {/* end button part */}
+        <div className={styles.submitBtn}>
+          <button
+            onClick={async () => {
+              const res = await signIn("credentials", {
+                email,
+                password,
+                redirect: false,
+              });
 
-          if (res?.status !== 200) {
-            toast({
-              title: "Wrong Email Or Password ",
-              status: "error",
-              duration: 5000,
-              isClosable: true,
-            });
-          } else {
-            router.push("/");
-          }
-        }}
-      >
-        Sign in
-      </button>
+              console.log(res);
+
+              if (res?.status !== 200) {
+                toast({
+                  title: "Wrong Email Or Password ",
+                  status: "error",
+                  duration: 5000,
+                  isClosable: true,
+                });
+              } else {
+                router.push("/");
+              }
+            }}
+          >
+            Sign in
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
