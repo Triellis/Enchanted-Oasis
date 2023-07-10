3d1417816791ec5d9ea9e7397da2ad1f0ed235a2
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-10 19:58:31 +0530

    feat: added themed greeting on the Dashboard

--- pages/Admin/Dashboard.tsx
+++ pages/Admin/Dashboard.tsx
@@ -1,13 +1,24 @@
 import { useSession } from "next-auth/react";
 import Layout from "../Layout";
 import { Button } from "@chakra-ui/react";
+import styles from "./Dashboard.module.css";
+
 export default function Admin() {
   const session = useSession();
+  let name = session.data?.user?.name;
   return (
     <>
       <Layout>
-        hello
-        <h1>{session.data?.user?.name}</h1>
+        {/* greetings part */}
+        <div className={styles.greeting}>
+      <span>
+        Welcome <span className={styles.name}>{name}</span>!{' '}
+      </span>
+      Let{' '}
+      <span className={styles.glowingText}>Lumos</span>{' '}
+      illuminate your path at our
+      <span className={styles.appName}> Enchanted Oasis!</span>
+    </div>
       </Layout>
     </>
   );
