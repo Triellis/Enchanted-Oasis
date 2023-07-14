03067cd0436b7eda8ec4925d3f42d243a687f7c3
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-07-15 00:15:48 +0530

    refactor: removed useless imports

--- pages/api/notification/index.ts
+++ pages/api/notification/index.ts
@@ -1,13 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
-import {
-  AdminNotificationCol,
-  HouseCol,
-  MySession,
-  UserCol,
-} from "../../../lib/types";
-import { clientPromise } from "../../../lib/DB";
+import { AdminNotificationCol, MySession, UserCol } from "@/lib/types";
+import { clientPromise } from "@/lib/DB";
 import { ObjectId, UpdateFilter } from "mongodb";
 
 export default async function handler(
