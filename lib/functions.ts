983143ae023cb139fbcc0733964738977fece6c1
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-07-17 12:56:36 +0530

    refactor: edit user function moved

--- lib/functions.ts
+++ lib/functions.ts
@@ -1,5 +1,5 @@
 import { Collection, MongoClient } from "mongodb";
-import { Role, UserCol } from "./types";
+import { Role, SentUserDataFromClient, UserCol } from "./types";
 import md5 from "md5";
 
 export async function validateLogin(
@@ -55,4 +55,45 @@ export function getRoleColor(role: Role) {
     roleColor = "green";
   }
   return roleColor;
+}
+export async function editUser(
+  newUserData: SentUserDataFromClient & { _id: string }
+) {
+  const formData = new FormData();
+  console.log(newUserData);
+
+  //allowed entries are name, profile picture, phone and password only:
+  const allowedEntries = [
+    "name",
+    "phone",
+    "password",
+    "profilePicture",
+    "userId",
+    "oldPicture",
+  ];
+
+  // the key should only consider those entries which are not empty and allowed
+
+  for (let key in newUserData) {
+    if (
+      allowedEntries.includes(key) &&
+      newUserData[key as keyof SentUserDataFromClient]
+    ) {
+      // @ts-ignore
+      formData.append(key, newUserData[key]);
+    }
+  }
+  if (!formData.get("profilePicture")) {
+    formData.delete("oldPicture");
+  }
+  if (newUserData.hasOwnProperty("_id")) {
+    formData.append("userId", newUserData._id);
+  }
+
+  const res = await fetch("/api/user", {
+    method: "PUT",
+    body: formData,
+  });
+
+  return res;
 }
