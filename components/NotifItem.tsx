e7b2a2409910c29a839cbabaaf168ed31e93ef91
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-18 14:41:00 +0530

    feat:  tweets 😄!

--- components/NotifItem.tsx
+++ components/NotifItem.tsx
@@ -13,6 +13,7 @@ import {
   ModalHeader,
   ModalOverlay,
   useDisclosure,
+  ModalFooter,
 } from "@chakra-ui/react";
 import React from "react";
 import styles from "./NotifItem.module.css";
@@ -26,12 +27,16 @@ export default function NotifItem() {
       <Avatar src="https://bit.ly/sage-adebayo" size="lg" />
       <Box fontSize="1.2em">
         {/* Name */}
-        <Text fontWeight="bold">Segun Adebayo </Text>
+        <Text fontWeight="bold">
+          Segun Adebayo
+          {/* Badge */}
+          {` `}
+          <Badge colorScheme="green">New</Badge>
+        </Text>
         {/* Email */}
         <Text fontSize="sm">admin@gmail.com</Text>
       </Box>
-      {/* Badge */}
-      <Badge colorScheme="green">New</Badge> {/* Content */}
+      {/* Content */}
       <Box className={styles.infoBox}>
         {/* title */}
         <Text fontSize="sm" fontWeight="bold" className={styles.title}>
@@ -57,14 +62,49 @@ export default function NotifItem() {
         onClose={onClose}
         isOpen={isOpen}
         motionPreset="slideInBottom"
+        size={{ base: "xs", sm: "lg", lg: "2xl" }}
       >
         <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
+        <ModalContent bg="hsl(var(--b3))">
+          <ModalHeader>
+            {/* Avatar, name, email */}
+            <Flex>
+              <Avatar src="https://bit.ly/sage-adebayo" />
+              <Box ml="3">
+                <Text fontWeight="bold">Segun Adebayo</Text>
+                <Text fontSize="sm" color="gray">
+                  UI Engineer
+                </Text>
+              </Box>
+            </Flex>
+          </ModalHeader>
           <ModalBody>
-            <Text>Modal Content</Text>
+            {/* Title and Content */}
+            <Box>
+              <Text fontWeight="bold" fontSize="1.2em">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam
+              </Text>
+              <Text fontSize="md ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam
+              </Text>
+            </Box>
           </ModalBody>
+          <ModalFooter justifyContent="flex-start" display="flex" pt="2">
+            {/* Date and time without icons */}
+            <Box>
+              <Box>
+                <Text fontSize="sm" color="gray">
+                  12:00pm • 12/12/2021
+                </Text>
+              </Box>
+              <Box>
+                {/* Badge */}
+                <Badge colorScheme="green">New</Badge>
+              </Box>
+            </Box>
+          </ModalFooter>
         </ModalContent>
       </Modal>
     </li>
