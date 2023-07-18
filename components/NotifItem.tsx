d486548a22106f8003ea088977ea89af05b9d406
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-07-18 16:29:28 +0530

    style: tweet type styling

--- components/NotifItem.tsx
+++ components/NotifItem.tsx
@@ -1,71 +1,55 @@
-import {
-  Flex,
-  Avatar,
-  Badge,
-  Text,
-  Box,
-  IconButton,
-  Tag,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-  ModalFooter,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-} from "@chakra-ui/react";
+import { Flex, Avatar, Badge, Text, Box, Spacer } from "@chakra-ui/react";
 import React from "react";
 import styles from "./NotifItem.module.css";
-import { CalendarIcon, EmailIcon, TimeIcon } from "@chakra-ui/icons";
+import { CalendarIcon, EmailIcon, TimeIcon, ViewIcon } from "@chakra-ui/icons";
 
 export default function NotifItem() {
   return (
-    <li style={{ listStyle: "none" }}>
-      <Box className={styles.notifItem} bg="hsl(var(--b3))">
-        {/* Modal for opening the notification */}
-        <Box>
-          {/* Avatar, name, email */}
-          <Flex>
-            <Avatar src="https://bit.ly/sage-adebayo" />
-            <Box ml="3">
-              <Text fontWeight="bold">Segun Adebayo</Text>
-              <Text fontSize="sm" color="gray">
-                UI Engineer
-              </Text>
-            </Box>
-          </Flex>
-        </Box>
-        <Box>
-          {/* Title and Content */}
-          <Box>
-            <Text fontWeight="bold" fontSize="1.2em">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    <div className={styles.notifItem}>
+      {/* Modal for opening the notification */}
+      <div className={styles.header}>
+        {/* Avatar, name, email */}
+        <Flex gap={4}>
+          <Avatar src="https://bit.ly/sage-adebayo" />
+          <div>
+            <Text fontWeight="bold">Segun Adebayo</Text>
+            <Text fontSize="sm" color={"hsl(var(--nc)  )"}>
+              UI Engineer
             </Text>
-            <Text fontSize="md ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            </Text>
-          </Box>
-        </Box>
-        <Box pt="2">
-          {/* Date and time without icons */}
-          <Box>
-            <Box>
-              <Text fontSize="sm" color="gray">
-                12:00pm • 12/12/2021
-              </Text>
-            </Box>
-            <Box>
-              {/* Badge */}
-              <Badge colorScheme="green">New</Badge>
-            </Box>
-          </Box>
-        </Box>
-      </Box>
-    </li>
+          </div>
+        </Flex>
+      </div>
+      <div className={styles.body}>
+        {/* Title and Content */}
+        <span className={styles.title}>
+          <Text fontWeight="bold" fontSize="1.2em">
+            Hello Moms
+          </Text>
+        </span>
+        <Text fontSize="md" color={"hsl(var(--pc) /70% )"}>
+          I will come to your house and fuck your mom with her computer ...
+        </Text>
+      </div>
+      <div className={styles.footer}>
+        {/* Date and time without icons */}
+
+        <Text fontSize="sm" color="hsl(var(--nc) )">
+          12:00pm • 12/12/2021
+        </Text>
+
+        {/* Badge */}
+        <Flex>
+          <div className={styles.badgesWrapper}>
+            <Badge colorScheme="green">New</Badge>
+            <Badge colorScheme="blue"> Student</Badge>
+            <Badge colorScheme="pink"> custom label </Badge>
+          </div>
+          <Spacer />
+          <div className={styles.viewsDisplay}>
+            2.1M<span className={styles.viewsText}>views</span>
+          </div>
+        </Flex>
+      </div>
+    </div>
   );
 }
