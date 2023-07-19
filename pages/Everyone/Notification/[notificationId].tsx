ca012144feb9fb0ed567ca929f1e729e7e367c1b
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-19 16:58:47 +0000

    style: improved the styling on notif screen

--- pages/Everyone/Notification/[notificationId].tsx
+++ pages/Everyone/Notification/[notificationId].tsx
@@ -7,6 +7,7 @@ import styles from "./Notification.module.css";
 import ReactMarkdown from "react-markdown";
 import ChakraUIRenderer from "chakra-ui-markdown-renderer";
 import remarkGfm from "remark-gfm";
+import { Flex, Avatar, Badge, Box, Text, Divider } from "@chakra-ui/react";
 
 function useNotification(id: string) {
   const { data, error, isLoading, mutate } = useSWR(
@@ -27,8 +28,31 @@ function NotificationComponent({
   notification: Notification;
 }) {
   return (
-    <div>
-      <h1>{notification.title}</h1>
+    <div className={styles.notifMain} >
+      {/* title */}
+      <div className={styles.title}>{notification.title}</div>
+
+      {/* Profile */}
+      <div className={styles.notifProfile}>
+        <Avatar src="https://bit.ly/sage-adebayo" size="lg" />
+        <Box>
+          <Text fontWeight="bold">Segun Adebayo</Text>
+          <Text fontSize="sm" color="hsl(var(--nc))">
+            admin@gmail.com
+          </Text>
+        </Box>
+      </div>
+
+      <Divider />
+      <div className={styles.notifMisc}>
+        <div>
+          <Badge colorScheme="green">Published</Badge>
+        </div>
+        <div>69views</div>
+      </div>
+      <Divider />
+
+      {/* content of the notification */}
       <ReactMarkdown
         remarkPlugins={[remarkGfm]}
         className={styles.markDownArea}
