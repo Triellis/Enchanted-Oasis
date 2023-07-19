75714118d51c4795df167cc5cce5a46b4268e6d9
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-19 12:37:26 +0530

    feat: added basic modal to compose message

--- pages/Admin/Dashboard.tsx
+++ pages/Admin/Dashboard.tsx
@@ -1,7 +1,18 @@
 import { useSession } from "next-auth/react";
 import Layout from "../Layout";
 
-import { Button, IconButton } from "@chakra-ui/react";
+import {
+  Button,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { FiFeather } from "react-icons/fi";
 import styles from "./Dashboard.module.css";
 
@@ -11,6 +22,8 @@ import NotifList from "@/components/NotifList";
 export default function Admin() {
   const session = useSession();
   let name = session.data?.user?.name;
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <Layout>
@@ -23,16 +36,36 @@ export default function Admin() {
           path at our
           <span className={styles.appName}> Enchanted Oasis!</span>
         </div>
-
         <div className={styles.notifications}>
           <NotifList />
         </div>
-
         <IconButton
           aria-label="compose"
           className={styles.composeMsg}
           icon={<FiFeather />}
+          onClick={onOpen}
         />
+        {/* Compose mesaage modal */}
+
+        <Modal
+          isCentered
+          onClose={onClose}
+          isOpen={isOpen}
+          motionPreset="slideInBottom"
+        >
+          <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>hasdfasf</ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button variant="ghost">Secondary Action</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </Layout>
     </>
   );
