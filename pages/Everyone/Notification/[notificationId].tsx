01b50dff6570ab64d9c75d37d77a1c55337cc293
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-07-19 14:53:07 +0530

    chore: basic page to read the notification is ready

--- pages/Everyone/Notification/[notificationId].tsx
+++ pages/Everyone/Notification/[notificationId].tsx
@@ -1,12 +1,44 @@
+import { fetcher } from "@/lib/functions";
 import Layout from "@/pages/Layout";
 import { useRouter } from "next/router";
+import useSWR from "swr";
+
+function useNotification(id: string) {
+  const { data, error, isLoading, mutate } = useSWR(
+    `/api/notification/${id}`,
+    fetcher
+  );
+  return {
+    notification: data as Notification,
+    isLoading,
+    error: error,
+    mutate,
+  };
+}
 
 export default function notificationsPage() {
   const router = useRouter();
+  const { notification, isLoading, error } = useNotification(
+    router.query.notificationId as string
+  );
+  let notificationComponent;
+  if (isLoading) {
+    notificationComponent = <div>Loading...</div>;
+  } else if (error) {
+    notificationComponent = <div>Error: {error}</div>;
+  } else {
+    notificationComponent = (
+      <div>
+        <h1>{notification.title}</h1>
+        <p>{notification.body}</p>
+      </div>
+    );
+  }
+
   return (
     <Layout>
       <div>
-        <h1>{router.query.notificationId}</h1>
+        <h1>{notificationComponent}</h1>
       </div>
     </Layout>
   );
