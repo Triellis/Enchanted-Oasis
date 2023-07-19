3812529ee9eec301bca266dcb2d103550313a3e6
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-19 12:07:14 +0000

    feat: added color swatches which are nt functional

--- pages/Admin/Dashboard.tsx
+++ pages/Admin/Dashboard.tsx
@@ -3,6 +3,7 @@ import Layout from "../Layout";
 
 import {
   Button,
+  Flex,
   IconButton,
   Input,
   Modal,
@@ -33,6 +34,174 @@ import styles from "./Dashboard.module.css";
 
 import NotifItem from "@/components/NotifItem";
 import NotifList from "@/components/NotifList";
+import classNames from "classnames";
+
+function ColorSwatch() {
+  const colors: string[] = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "DarkOrange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "RebeccaPurple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen",
+  ]; // Add different colors here
+  return (
+    <div className={styles.colorPalette}>
+      {colors.map((color, index) => (
+        <Button
+          key={index}
+          className={classNames("clicky", styles.swatch)}
+          style={{ backgroundColor: color }}
+          onClick={() => {
+            console.log(color);
+          }}
+        ></Button>
+      ))}
+    </div>
+  );
+}
 
 function ComposeMsgModal({
   isOpen,
@@ -77,13 +246,12 @@ function ComposeMsgModal({
 
               <Popover>
                 <PopoverTrigger>
-                  <Button></Button>
+                  <Button className="clicky"></Button>
                 </PopoverTrigger>
-                <PopoverContent className={styles.colorPalette}>
-                  <PopoverArrow bg="hsl(var(--b3))" />
-                  <PopoverHeader>Confirmation!</PopoverHeader>
-                  <PopoverBody>
-                    Are you sure you want to have that milkshake?
+                <PopoverContent className={styles.popContent}>
+                  <PopoverBody p="0em">
+                    Choose color
+                    <ColorSwatch />
                   </PopoverBody>
                 </PopoverContent>
               </Popover>
