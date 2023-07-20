bea56fe12add5ed1a587a5f01d4f573c3240c3f1
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-20 06:11:09 +0000

    style: added icons to sidebar menu

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -2,6 +2,40 @@ import Link from "next/link";
 import styles from "./SidebarItem.module.css";
 import { Router, useRouter } from "next/router";
 import classNames from "classnames";
+
+import { RxDashboard } from "react-icons/rx";
+import { FiUsers } from "react-icons/fi";
+import { BsHouses } from "react-icons/bs";
+import { IoLibraryOutline } from "react-icons/io5";
+
+function IconPrev({ text }: { text: string }) {
+  if (text === "Dashboard") {
+    return (
+      <div className={styles.icon}>
+        <RxDashboard />
+      </div>
+    );
+  } else if (text === "Users") {
+    return (
+      <div className={styles.icon}>
+        <FiUsers />
+      </div>
+    );
+  } else if (text === "Houses") {
+    return (
+      <div className={styles.icon}>
+        <BsHouses />
+      </div>
+    );
+  } else if (text === "Courses") {
+    return (
+      <div className={styles.icon}>
+        <IoLibraryOutline />
+      </div>
+    );
+  }
+}
+
 export default function SidebarItem({
   text,
   linkTo,
@@ -20,6 +54,7 @@ export default function SidebarItem({
         )}
         href={linkTo}
       >
+        <IconPrev text={text} />
         {text}
       </Link>
     </div>
