d072c6118c5519c61805bc96304dad04254f15b4
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-20 11:07:13 +0000

    feat: added tabs which are not functional yet #101

--- components/NotifList.tsx
+++ components/NotifList.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 import Pagination from "./Pagination";
 
 import styles from "./NotifList.module.css";
-import { Divider } from "@chakra-ui/react";
+import { Divider, Tab, TabIndicator, TabList, Tabs } from "@chakra-ui/react";
 
 function useNotifications(page: number, unseenOnly: boolean) {
   const { data, error, mutate } = useSWR(
@@ -23,8 +23,51 @@ function useNotifications(page: number, unseenOnly: boolean) {
   };
 }
 
+interface TabsComponentProps {
+  inbox: string;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+  setInbox: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function InboxTabs({ inbox, setPage, setInbox }: TabsComponentProps) {
+  const tabs = [
+    { label: "All", inbox: "All" },
+    { label: "Unseen", inbox: "Unseen" },
+  ];
+
+  return (
+    <Tabs
+      className={styles.tabGrp}
+      position="relative"
+      variant="unstyled"
+      defaultIndex={0}
+      onChange={(index) => {
+        if (index === 0) {
+          setInbox("All");
+        } else if (index === 1) {
+          setInbox("Unseen");
+        }
+        setPage(1); // Set the page state to 1 on click
+      }}
+    >
+      <TabList className={styles.tabList}>
+        {tabs.map((tab, index) => (
+          <Tab key={index}>{tab.label}</Tab>
+        ))}
+        <TabIndicator
+          zIndex={-1}
+          className={styles.tabIndicator}
+          backgroundColor={inbox === "Unseen" ? "red.600" : "gray.600"}
+        />
+      </TabList>
+    </Tabs>
+  );
+}
+
 export default function NotifList() {
   const [page, setPage] = useState(1);
+  const [inbox, setInbox] = useState("All");
+
   const [unseenOnly, setUnseenOnly] = useState(false);
   const { notifications, isLoading, isError, mutate } = useNotifications(
     page,
@@ -56,6 +99,11 @@ export default function NotifList() {
         <h1 className={styles.title}>Admin Notifications</h1>
       </div>
       <Divider />
+
+      {/* tabs here */}
+      <div className={styles.wrapTab} >
+        <InboxTabs inbox={inbox} setPage={setPage} setInbox={setInbox} />
+      </div>
 
       <div className={styles.notifList}>{componentToRender}</div>
       <br />
