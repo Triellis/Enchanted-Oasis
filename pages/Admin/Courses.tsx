a7f9d379cdfbfd61bfe2c8c95552317aa1dac743
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-24 15:57:56 +0530

    feat: basic modal implemented (#164)

--- pages/Admin/Courses.tsx
+++ pages/Admin/Courses.tsx
@@ -8,7 +8,17 @@ import SearchBar from "@/components/SearchBar/SearchBar";
 import Pagination from "@/components/Pagination/Pagination";
 import styles from "./Courses.module.css";
 import classNames from "classnames";
-import { Button } from "@chakra-ui/react";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 function useCourses(
   page: number,
@@ -27,10 +37,40 @@ function useCourses(
   };
 }
 
+function AddCourseModal({ isOpen, onClose, onOpen }: any) {
+  return (
+    <>
+      <Modal
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>asdkjflasfdhkjh</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
 export default function Courses() {
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState("");
   const { courses, isLoading, error, mutate } = useCourses(page, search, "all");
+
+  // for the modal:
+  const {
+    isOpen: isAddCourseOpen,
+    onOpen: onAddCourseOpen,
+    onClose: onAddCourseClose,
+  } = useDisclosure();
 
   let componentToRender;
   if (isLoading) {
@@ -40,6 +80,7 @@ export default function Courses() {
   } else {
     componentToRender = <CourseList courses={courses} />;
   }
+
   return (
     <>
       <Layout>
@@ -48,10 +89,19 @@ export default function Courses() {
           {componentToRender}
           <Pagination items={courses} page={page} setPage={setPage} />
 
-          <Button className={classNames(styles.courseAdd, "clicky")}>
+          <Button
+            className={classNames(styles.courseAdd, "clicky")}
+            onClick={onAddCourseOpen}
+          >
             Add course
           </Button>
         </div>
+
+        <AddCourseModal
+          isOpen={isAddCourseOpen}
+          onClose={onAddCourseClose}
+          onOpen={onAddCourseOpen}
+        />
       </Layout>
     </>
   );
