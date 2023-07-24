280c2fd9e4a300d298a0371c0aa416b74e392fee
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-24 17:10:09 +0530

    feat: some entries added in the modal, responsive so far (#164)

--- components/AddCourseModal/AddCourseModal.tsx
+++ components/AddCourseModal/AddCourseModal.tsx
@@ -1,5 +1,9 @@
 import {
   Button,
+  Divider,
+  FormControl,
+  FormLabel,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -7,10 +11,57 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Textarea,
 } from "@chakra-ui/react";
 
 import React from "react";
 import styles from "./AddCourseModal.module.css";
+import classNames from "classnames";
+
+// component to select days for the course
+function Days() {
+  return (
+    <div className={styles.days}>
+      <div className={classNames(styles.day, styles.selected)}>M</div>
+    </div>
+  );
+}
+
+// component to add times to the course
+function Time() {}
+
+// component for the form for the course information:
+function CourseInfo() {
+  return (
+    <div className={styles.info}>
+      {/* course name */}
+      <FormControl>
+        <FormLabel>Course Name</FormLabel>
+        <Input placeholder="Enter Course Name" />
+      </FormControl>
+
+      <div className={styles.infoSub}>
+        {/* course code */}
+        <FormControl>
+          <FormLabel>Course Name</FormLabel>
+          <Input placeholder="Enter Course Name" />
+        </FormControl>
+
+        {/* credits */}
+        <FormControl>
+          <FormLabel>Course Name</FormLabel>
+          <Input placeholder="Enter Course Name" />
+        </FormControl>
+      </div>
+
+      {/* course description */}
+      <div className={styles.infoDes}>
+        <FormLabel>Description</FormLabel>
+        <Textarea placeholder="Here is a sample placeholder" />
+      </div>
+    </div>
+  );
+}
 
 export default function AddCourseModal({ isOpen, onClose, onOpen }: any) {
   return (
@@ -20,14 +71,29 @@ export default function AddCourseModal({ isOpen, onClose, onOpen }: any) {
         isOpen={isOpen}
         isCentered
         motionPreset="slideInBottom"
+        size={{ sm: "2xl", base: "xs", lg: "3xl" }}
+        scrollBehavior={"inside"}
       >
         <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>asdkjflasfdhkjh</ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose}>Close</Button>
+        <ModalContent bg="hsl(var(--b1))">
+          {/* header */}
+          <ModalHeader>Add Course</ModalHeader>
+
+          {/* modal content */}
+          <ModalBody className={styles.modalBody}>
+            <CourseInfo />
+            <Divider className={styles.divider} />
+            <FormLabel>Schedule</FormLabel>
+          </ModalBody>
+
+          {/* footer */}
+          <ModalFooter className={styles.modalFooter}>
+            <Button variant={"outline"} onClick={onClose}>
+              Close
+            </Button>
+            <Button onClick={() => {}} bg="hsl(var(--s))">
+              Add Course
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
