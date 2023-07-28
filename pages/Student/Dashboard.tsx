2d65a182aa69da324624685cce3a08ff406b7e61
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-28 21:41:47 +0530

    feat: added joke to student and faculty dashboard

--- pages/Student/Dashboard.tsx
+++ pages/Student/Dashboard.tsx
@@ -1,9 +1,11 @@
 import NotifList from "@/components/NotifList";
 import Layout from "../Layout";
+import Joke from "@/components/Joke/Joke";
 
 export default function Dashboard() {
   return (
     <Layout>
+      <Joke />
       <NotifList />
     </Layout>
   );
