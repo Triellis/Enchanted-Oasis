0c1816a4c3b4edbf79310239a52f933c103f772b
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-29 19:09:17 +0530

    chore: adding more on top of basic code

--- components/FloatingButton/FloatingButton.tsx
+++ components/FloatingButton/FloatingButton.tsx
@@ -1,6 +1,38 @@
 import React from "react";
 import styles from "./FloatingButton.module.scss";
 
-export default function FloatingButton() {
-  return <div>FloatingButton</div>;
+export default function FloatingButton({
+  onOpen,
+  SideIcon,
+  HalfText,
+  initialWidth,
+  finalWidth,
+}: {
+  onOpen: () => void;
+  SideIcon: any;
+  HalfText: string;
+  RemainingText: string;
+  initialWidth: number;
+  finalWidth: number;
+}) {
+  // declaring variable styles for the button
+  const floatBtnStyle = {
+    width: initialWidth,
+    transition: "width 0.5s",
+  };
+
+  return (
+    <div>
+      <button
+        style={floatBtnStyle}
+        className={styles.floatBtn}
+        onClick={() => {
+          onOpen();
+        }}
+      >
+        <SideIcon className={styles.icon} />
+        {HalfText}{" "}
+      </button>
+    </div>
+  );
 }
