e4ebfa653088c8332b15daefbb2fe5da5c30543d
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-07-29 15:21:53 +0530

    style: loader is updated to accent spinner

--- components/Joke/Joke.tsx
+++ components/Joke/Joke.tsx
@@ -1,6 +1,7 @@
 import useSWR from "swr";
 import styles from "./Joke.module.css";
 import MotionDiv from "../MotionDiv";
+import { Spinner } from "@chakra-ui/react";
 
 function useJoke() {
   const { data, error, isLoading, mutate } = useSWR(
@@ -33,24 +34,7 @@ function JokeComponent({ joke, mutate }: { joke: string; mutate: any }) {
 function Loader() {
   const balls = [{ delay: 0 }, { delay: 0.1 }, { delay: 0.2 }];
 
-  return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      {balls.map((ball, index) => (
-        <MotionDiv
-          key={index}
-          style={{
-            width: "0.5em",
-            height: "0.5em",
-            borderRadius: "50%",
-            backgroundColor: "hsl(var(--nc))",
-            margin: "1px",
-          }}
-          animate={{ y: [0, -20, 0], scale: [1, 0.8, 1] }}
-          transition={{ duration: 1, repeat: Infinity, delay: ball.delay }}
-        />
-      ))}
-    </div>
-  );
+  return <Spinner color="hsl(var(--a))" />;
 }
 
 export default function Joke() {
@@ -65,9 +49,5 @@ export default function Joke() {
     componentsToRender = <JokeComponent joke={joke!} mutate={mutate} />;
   }
 
-  return (
-    <div className={styles.joke}>
-      {componentsToRender}
-    </div>
-  );
+  return <div className={styles.joke}>{componentsToRender}</div>;
 }
