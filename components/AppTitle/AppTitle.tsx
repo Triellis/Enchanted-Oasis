ceeb166ac8942f2e5299544b8adf640bf9197cda
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-08-01 09:58:43 +0700

    chore: code cleanup

--- components/AppTitle/AppTitle.tsx
+++ components/AppTitle/AppTitle.tsx
@@ -23,5 +23,3 @@ export default function AppTitle({ sizeNumber }: { sizeNumber?: number }) {
     </div>
   );
 }
-// git config --global user.email "you@example.com"
-// git config --global user.name "Your Name"
