4771a449cfbe15b966686797e6dd962074289f48
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-08-01 09:57:37 +0700

    feat: i think this is helping to go back to dashboard

--- components/AppTitle/AppTitle.tsx
+++ components/AppTitle/AppTitle.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import styles from "./AppTitle.module.css";
+import { useRouter } from "next/router";
 
 export default function AppTitle({ sizeNumber }: { sizeNumber?: number }) {
   const titleSize = {
     fontSize: `${sizeNumber}em`,
   };
+
+  const router = useRouter();
+
   return (
     <div className={styles.appTitle}>
       <button
         style={titleSize}
         className={styles.appName}
         onClick={() => {
-          window.location.href = "/";
+          router.push("/");
         }}
       >
         Enchanted Oasis
