73819aeef3cc357abdff27ececeeb1f69bf87801
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-08-05 14:09:05 +0530

    fix: redirect

--- components/AppTitle/AppTitle.tsx
+++ components/AppTitle/AppTitle.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./AppTitle.module.css";
 import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import { MySession } from "@/lib/types";
 
 export default function AppTitle({ sizeNumber }: { sizeNumber?: number }) {
   const titleSize = {
@@ -8,6 +10,7 @@ export default function AppTitle({ sizeNumber }: { sizeNumber?: number }) {
   };
 
   const router = useRouter();
+  const session = useSession().data as MySession;
 
   return (
     <div className={styles.appTitle}>
@@ -15,7 +18,7 @@ export default function AppTitle({ sizeNumber }: { sizeNumber?: number }) {
         style={titleSize}
         className={styles.appName}
         onClick={() => {
-          router.push("/");
+          router.push(`/${session?.user.role}/Dashboard`);
         }}
       >
         Enchanted Oasis
